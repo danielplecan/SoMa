5e62417a4d29084c6585a1645130a07304c683bd
danielplecan <daniel.plecan@gmail.com>
2015-01-26 10:11:24 +0200

    Twitter service implementation

--- public_html/js/twitterService.js
+++ public_html/js/twitterService.js
@@ -2,14 +2,16 @@
     OAuth.initialize('NUzclKmeAT45GJaDVan7gJI9s1g');
     
     OAuth.popup('twitter', {cache: true}).done(function (twitter) {
-        alert("IEEEI");
         console.log(twitter);
         twitter.get("/1.1/search/tweets.json?q=%23superbowl&result_type=recent").done(function (data) {
-            console.log(data.statuses[0].id);
-            twitter.get("https://api.twitter.com/1.1/statuses/oembed.json?id=507185938620219395").done(function(data){
-                console.log(data.html);
-                $("#mashup").html(data.html);
-            });
+            
+            for(var i = 0; i < data.statuses.length; i++) {
+                twitter.get("/1.1/statuses/oembed.json?id=" + data.statuses[i].id + "&hide_media=true&hide_thread=true").done(function (data) {
+                    var tweet = $("<div class='panel-body' />");
+                    $(tweet).html(data.html);
+                    $("#tweets").append(tweet);
+                });
+            }
             
         }).fail(function (err) {
             console.log(err);
