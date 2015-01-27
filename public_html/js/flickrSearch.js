1db4ef25da7fe0a10c5f90a0d79b7259cfe8d570
luciancostinailenei <luciancostin.ailenei@gmail.com>
2015-01-27 02:27:16 +0200

    Images Title and Link onMouseOver

--- public_html/js/flickrSearch.js
+++ public_html/js/flickrSearch.js
@@ -19,10 +19,14 @@ function flickrSearch() {
                 var basePhotoURL = 'http://farm' + rPhoto.farm + '.static.flickr.com/'
                     + rPhoto.server + '/' + rPhoto.id + '_' + rPhoto.secret;
 
+                console.log(rPhoto);
+
+                // var photoTitle = rPhoto.title;
                 var originalPhotoURL = basePhotoURL + "_o.jpg"
                 var mashupPhotoURL = basePhotoURL + '_q.jpg';
-                var tabPhotoURL = basePhotoURL + '_m.jpg';
+                var PhotoURL = basePhotoURL + '_z.jpg';
 
+                /*
                 var photoStringStart = '<a ';
                 var photoStringMashupEnd = 'title="' + rPhoto.title + '" href="'+
                     originalPhotoURL +'"><img src="' + mashupPhotoURL + '" class="mashup-images-panel-element" alt="' +
@@ -33,19 +37,42 @@ function flickrSearch() {
 
                 var photoStringMashup;
                 var photoStringTab;
+                */
+
+                var imgContTab = '<div class="tab-images-panel-element" style="background: url(' + mashupPhotoURL + ');"><div class="image-info"><p class="top"><a class="title" href="' + PhotoURL + '">' + rPhoto.title + '</p></div></div>';
+                var imgContMashup = '<div class="mashup-images-panel-element" style="background: url(' + mashupPhotoURL + ');"><div class="image-info"><p class="top"><a class="title" href="' + PhotoURL + '">' + rPhoto.title + '</p></div></div>';
 
                 if(i < showOnPageMashup ){
 
-                photoStringMashup =  photoStringStart + photoStringMashupEnd;
-                $(photoStringMashup).appendTo("#mashup-images-panel");
+                //photoStringMashup =  photoStringStart + photoStringMashupEnd;
+                    $(imgContMashup).appendTo('#mashup-images-panel');
+                //$(photoStringMashup).appendTo("#mashup-images-panel");
 
                 }
 
                 if(i < showOnPageTab ){
-                    photoStringTab = photoStringStart +  photoStringTabEnd;
-                    $(photoStringTab).appendTo("#mashup-images-panel-tab-flickr");
+                    //photoStringTab = photoStringStart +  photoStringTabEnd;
+                    //$(photoStringTab).appendTo("#mashup-images-panel-tab-flickr");
+                    $(imgContTab).appendTo('#mashup-images-panel-tab-flickr');
                 }
 
             });
         });
 };
+
+$(document).on("mouseover", '.mashup-images-panel-element', function(){
+    $(this).children('div').attr('class', 'image-info-active-mashup');
+});
+
+$(document).on('mouseout', '.mashup-images-panel-element', function(){
+    $(this).children('div').attr('class', 'image-info');
+});
+
+$(document).on("mouseover", '.tab-images-panel-element', function(){
+    $(this).children('div').attr('class', 'image-info-active-tab');
+});
+
+$(document).on('mouseout', '.tab-images-panel-element', function(){
+    $(this).children('div').attr('class', 'image-info');
+});
+
