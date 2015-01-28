d2ead1869852a714b06e52257df23822220229f1
danielplecan <daniel.plecan@gmail.com>
2015-01-28 10:57:09 +0200

    Final commit

--- public_html/js/twitterService.js
+++ public_html/js/twitterService.js
@@ -1,25 +1,50 @@
 function twitterSearch(query) {
     OAuth.initialize('NUzclKmeAT45GJaDVan7gJI9s1g');
-    
+
     var twitter = OAuth.create('twitter');
-    
-    if(twitter !== false) {
+
+    if (twitter !== false) {
         searchTweets(twitter, query);
     } else {
-        OAuth.popup('twitter', {cache: true})
+        $("#twitterLoginBtn").removeClass("hidden");
+    }
+}
+
+function twitterLogin() {
+    OAuth.popup('twitter', {cache: true})
             .done(function (twitter) {
-                searchTweets(twitter, query);
+                alert("Twitter authentication successful. You can now use this provider during your searches.");
+                $("#twitterLoginBtn").addClass("hidden");
             })
             .fail(function () {
                 alert("Twitter authentication failed. Cannot provide results from this social media provider. Please try again.");
+                $("#twitterLoginBtn").removeClass("hidden");
             });
-    }
+
 }
+
+$(document).ready(function() {
+    OAuth.initialize('NUzclKmeAT45GJaDVan7gJI9s1g');
+    
+    $(document).on('click', '#twitterLoginBtn', function () {
+        event.preventDefault();
+        twitterLogin();
+    });
+    
+
+    var twitter = OAuth.create('twitter');
+
+    if (twitter !== false) {
+        $("#twitterLoginBtn").addClass("hidden");
+    } else {
+        $("#twitterLoginBtn").removeClass("hidden");
+    }
+});
 
 function searchTweets(twitter, keywords) {
     keywords = keywords.replace(" ", " OR ");
     keywords = encodeURI(keywords);
-    
+
     twitter.get("/1.1/search/tweets.json?q=" + keywords + "&result_type=recent")
             .done(function (data) {
                 renderTweets(twitter, data);
@@ -34,12 +59,12 @@ function renderTweets(twitter, data) {
         var url = "https://api.twitter.com/1.1/statuses/oembed.json?id="
                 + data.statuses[i].id +
                 "&hide_media=true&hide_thread=true";
-        
+
         twitter.get(url).done(function (data) {
             var tweet = $("<li />");
             $(tweet).html(data.html);
             $("#mashup-tweets-list").append(tweet);
-            
+
             var tweet = $("<li />");
             $(tweet).html(data.html);
             $("#mashup-twitter-stories-list").append(tweet);
